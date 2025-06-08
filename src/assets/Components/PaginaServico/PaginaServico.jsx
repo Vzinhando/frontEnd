81c6aee9a0ec07192d66500c58eddb6d626a905e
Breno Alves <biomassa141@gmail.com>
2025-06-08 00:22:58 -0300

    att by breno

--- src/assets/Components/PaginaServico/PaginaServico.jsx
+++ src/assets/Components/PaginaServico/PaginaServico.jsx
@@ -3,12 +3,14 @@ import "./PaginaServico.css";
 function PaginaServico() {
   return (
     <>
+    <section className="paginaServicoMain">
+
       <div className="telasPaginaServico">
         <div className="esquerdaPaginaServico">
-          <div className="imagem-esquerdaPaginaServico">
+          <div className="imagemEsquerdaPaginaServico">
             <img src="/img/ImagemLaura/Logo4.svg" alt="" />
           </div>
-          <div className="esquerda-textoPagina">
+          <div className="esquerdaTextoPagina">
             <p>
               Mostre ao bairro o que <br /> você faz de melhor!
             </p>
@@ -17,20 +19,21 @@ function PaginaServico() {
 
 
         <div className="direitaPaginaServico">
-          <div className="texto">
+          <div className="textoPaginaServico">
             <h2>Descreva seu serviço</h2>
             <p>Compartilhe informações básicas,<br/> como local de atuação e seu serviço.</p>
           </div>
-          <div className="texto">
+          <div className="textoPaginaServico">
             <h2>Se destaque na vizinhança</h2>
             <p>Adicione fotos, experiências <br/> anteriores e descrição ao seu perfil.</p>
           </div>
-          <div className="texto">
+          <div className="textoPaginaServico">
             <h2>Comece no Vizinhando!</h2>
             <p>Escolha os preço inicial, verifique as <br/> informações e publique seus serviços.</p>
           </div>
         </div>
       </div>
+    </section>
     </>
   );
 }
