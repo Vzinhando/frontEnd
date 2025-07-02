63156076ded167e637f1bf60636d92cf490c8f5f
Breno Alves <biomassa141@gmail.com>
2025-07-02 11:18:21 -0300

    add .jsx

--- src/assets/Components/PaginaPrincipal/PaginaPrincipal.jsx
+++ src/assets/Components/PaginaPrincipal/PaginaPrincipal.jsx
@@ -1,15 +1,15 @@
 import React, { lazy, Suspense } from 'react'
 import "./PaginaPrincipal.css";
 //COMPONENTES ACIMA DA DOBRA (CARREGAMENTO NORMAL E IMEDIATO)
-import Hero from "./Hero/Hero";
-import Carrossel from "./Carrossel/Carrossel";
+import Hero from "./Hero/Hero.jsx";
+import Carrossel from "./Carrossel/Carrossel.jsx";
 // Todos estes componentes só serão baixados quando o usuário rolar a página
-const BlocoChatBot = lazy(() => import("./BlocoChatBot/BlocoChatBot"));
-const BlocoServicoTrabalho = lazy(() => import("./BlocoServicoTrabalho/BlocoServicoTrabalho"));
-const BlocoMulher = lazy(() => import("./BlocoMulher/BlocoMulher"));
-const Duvida = lazy(() => import("./BlocoDuvida/Duvida"));
-const AgrupamentoCardAvaliacao = lazy(() => import("./AvaliacoesHome/CardAvaliacao/AgrupamentoCardAvaliacao"));
-const Assinatura = lazy(() => import("./BlocoClube/Assinatura"));
+const BlocoChatBot = lazy(() => import("./BlocoChatBot/BlocoChatBot.jsx"));
+const BlocoServicoTrabalho = lazy(() => import("./BlocoServicoTrabalho/BlocoServicoTrabalho.jsx"));
+const BlocoMulher = lazy(() => import("./BlocoMulher/BlocoMulher.jsx"));
+const Duvida = lazy(() => import("./BlocoDuvida/Duvida.jsx"));
+const AgrupamentoCardAvaliacao = lazy(() => import("./AvaliacoesHome/CardAvaliacao/AgrupamentoCardAvaliacao.jsx"));
+const Assinatura = lazy(() => import("./BlocoClube/Assinatura.jsx"));
 
 const Loading = () => <div style={{height: '100px'}}></div>;
 
