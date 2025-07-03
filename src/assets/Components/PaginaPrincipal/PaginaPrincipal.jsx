14c06777bdb0c3d9e07d6a9dcbf71d864c44ff43
FelipeSouza6k <125583116+FelipeSouza6k@users.noreply.github.com>
2025-07-03 18:10:34 -0300

    Adicionando data-aos

--- src/assets/Components/PaginaPrincipal/PaginaPrincipal.jsx
+++ src/assets/Components/PaginaPrincipal/PaginaPrincipal.jsx
@@ -3,6 +3,7 @@ import "./PaginaPrincipal.css";
 //COMPONENTES ACIMA DA DOBRA (CARREGAMENTO NORMAL E IMEDIATO)
 import Hero from "./Hero/Hero";
 import Carrossel from "./Carrossel/Carrossel";
+
 // Todos estes componentes só serão baixados quando o usuário rolar a página
 const BlocoChatBot = lazy(() => import("./BlocoChatBot/BlocoChatBot"));
 const BlocoServicoTrabalho = lazy(() => import("./BlocoServicoTrabalho/BlocoServicoTrabalho"));
@@ -17,17 +18,37 @@ const Loading = () => <div style={{height: '100px'}}></div>;
 function PaginaPrincipal() {
     return (
         <main>
+            {/* Componentes do topo não precisam de animação de scroll */}
             <Hero />
             <Carrossel />
-            {/*Suspense envolve todos os componentes lazy-loaded */}
-            {/* O fallback pode ser um spinner, um esqueleto da UI, ou simplesmente nada */}
+
             <Suspense fallback={<Loading />}>
-                <BlocoChatBot />
-                <BlocoServicoTrabalho />
-                <BlocoMulher />
-                <Duvida />
-                <AgrupamentoCardAvaliacao />
-                <Assinatura />
+                {/* Envolvemos cada componente lazy-loaded em uma div com data-aos */}
+
+                <div data-aos="fade-up">
+                    <BlocoChatBot />
+                </div>
+
+                <div data-aos="fade-up" data-aos-delay="100">
+                    <BlocoServicoTrabalho />
+                </div>
+
+                <div data-aos="fade-right">
+                    <BlocoMulher />
+                </div>
+                
+                <div data-aos="fade-left">
+                    <Duvida />
+                </div>
+
+                <div data-aos="zoom-in">
+                    <AgrupamentoCardAvaliacao />
+                </div>
+
+                <div data-aos="fade-up" data-aos-duration="1200">
+                    <Assinatura />
+                </div>
+
             </Suspense>
         </main>
     );
